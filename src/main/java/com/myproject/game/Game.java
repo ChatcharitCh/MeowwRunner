6cf2cf7ea4b08bf83dcae98c5883f7ca1b2d28f6
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-06-19 16:06:38 +0700

    Create Display and Lyaout

---
+++ src/main/java/com/myproject/game/Game.java
@@ -0,0 +1,27 @@
+
+
+package com.myproject.game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+
+public class Game extends JPanel {
+
+    public Game() {
+        this.setBounds(0, 0, 1000, 600);
+        this.setFocusable(true);
+        this.setLayout(null);
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.red);
+        g2.drawRect(50, 50, 50, 50);
+    }
+    
+}
